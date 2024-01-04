a78a36d17108862068cfcf92f36382526a2f34ff
Kaan <81617346+Kaanhehe@users.noreply.github.com>
2024-01-04 05:10:44 +0100

    Add display setup for running GUI tests

--- test_main.py
+++ test_main.py
@@ -3,10 +3,6 @@ import string
 from PyQt5.QtWidgets import QApplication
 from main import PasswordGenerator
 
-@pytest.fixture(scope='session')
-def qapp():
-    return QApplication([])
-
 @pytest.fixture
 def app(qapp, request):
     return PasswordGenerator()
